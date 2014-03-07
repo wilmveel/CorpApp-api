d035cf2635166c57d20fec1f0f13c4bea71baac8
Willem Veelenturf <willem.veelenturf@gmail.com>
2014-03-07 20:43:36 +0100

    Remove web.xml (reverted from commit 5c693dcc3ee2e7d0c6560cff96353e3741568cef)

--- src/main/java/nl/capgemini/corpapp/filter/CorsFilter.java
+++ src/main/java/nl/capgemini/corpapp/filter/CorsFilter.java
@@ -8,21 +8,23 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class CorsFilter implements Filter {
+import org.springframework.web.filter.OncePerRequestFilter;
 
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+public class CorsFilter extends OncePerRequestFilter  {
+
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws ServletException, IOException {
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
 		chain.doFilter(req, res);
+		
 	}
-
-	public void init(FilterConfig filterConfig) {}
-
-	public void destroy() {}
 
 }
